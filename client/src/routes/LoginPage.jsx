d4911f156a9bfb2a9773464a8f09065c9d9c1336
Alex <alexhwebdev@gmail.com>
2024-12-28 14:30:13 -0800

    Stopped at Clerk

---
+++ client/src/routes/LoginPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage
+
+// import { SignIn } from "@clerk/clerk-react";
+
+// const LoginPage = () => {
+//   return (
+//     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
+//       <SignIn signUpUrl="/register"/>
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
---
+++ client/src/routes/RegisterPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div>RegisterPage</div>
+  )
+}
+
+export default RegisterPage
+
+// import { SignUp } from "@clerk/clerk-react";
+
+// const RegisterPage = () => {
+//   return (
+//     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
+//       <SignUp signInUrl="/login" />
+//     </div>
+//   );
+// };
+
+// export default RegisterPage;
