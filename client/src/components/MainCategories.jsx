881f9c169bcfb1a16b4e655ba9818efb6dedec58
Alex <alexhwebdev@gmail.com>
2025-01-01 21:51:41 -0800

    clean up

--- client/src/components/MainCategories.jsx
+++ client/src/components/MainCategories.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Search from "./Search";
 
+
 const MainCategories = () => {
   return (
     <div className="hidden md:flex bg-white rounded-3xl xl:rounded-full p-4 shadow-lg items-center justify-center gap-8">
